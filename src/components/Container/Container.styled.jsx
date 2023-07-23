459cf796d3794c259abc38470b377685fd3e38d9
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-07-23 04:05:19 +0200

    Update Container.styled.jsx

--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const StyledContainer = styled.div`
-  margin: 0 auto;
   // background: ${({ theme }) => theme.color.gradient};
   // box-shadow: ${({ theme }) => theme.boxShadowHover};
   margin: auto;
