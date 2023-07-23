d0b953de318624c73e345220b6df428cb801ff2c
glorrymiss <113041245+glorrymiss@users.noreply.github.com>
2023-07-23 16:32:02 +0200

    update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,12 +1,15 @@
-import {Title, FieldStyled, FormStyled, Button, Text, Wrap, StyledNavLink, TextError} from './LoginForm.styled'
+import {Title, FieldStyled, FormStyled, Button, Text, Wrap, StyledNavLink, TextError, FieldError, SuccessText, IconCrossStyle, IconEyeClosedStyle} from './LoginForm.styled'
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { logIn } from 'redux/auth/operations';
 import { Formik} from 'formik';
 import * as Yup from 'yup'
+import theme from 'components/theme';
+import { useAuth } from 'hooks';
+
 
 const validationSchema = Yup.object().shape({
-    email:Yup.string().required("Field is a required").email('Enter a valid email'),
+    email:Yup.string().required("Field is a required").email('Enter a valid Email'),
     password:Yup.string().required("Field is a required")
     .min(6, "Password must be at least 8 characters")
     .max(16, "Password must be less at 16 characters")
@@ -22,10 +25,10 @@ const LoginForm = () => {
     const dispatch = useDispatch();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const {  currentTheme } = useAuth();
 
 
-
-    const handleLogInSubmit = event => {
+    const handleLogInSubmit =(values, event) => {
         event.preventDefault();
 
     dispatch(
@@ -34,6 +37,7 @@ const LoginForm = () => {
             password: password,
           })
         );
+        console.log(logIn);
         reset();
       };
 
@@ -51,7 +55,7 @@ return(
       }}
       validationSchema={validationSchema}
       onSubmit={values => {
-        handleLogInSubmit()
+        handleLogInSubmit(values)
         console.log("submit", values);
       }}>
          {({
@@ -65,28 +69,48 @@ return(
      <FormStyled  onSubmit={handleSubmit}>
         <Title>Login</Title>
         <Wrap>
-        <FieldStyled type="email"
+        {errors.email && touched.email && errors.email ? <label style={{position:'relative'}}><FieldError type="email"
             name="email"
             value={values.email}
             onChange={handleChange}
             onBlur={handleBlur}
-             placeholder='Email' 
-             required/>
-        <TextError>
-                  {errors.email && touched.email && errors.email}
-        </TextError>
-        <FieldStyled  type="password"
+             placeholder='Email'
+             required/><IconCrossStyle fill={theme[currentTheme].color.error}/>
+             <TextError>{errors.email && touched.email && errors.email}</TextError>
+             </label>:
+            <FieldStyled  type="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            style={{borderColor:touched.email? ({ theme }) => theme.color.indicator:({ theme }) => theme.color.btnDark}}
+             placeholder='Email'
+             required/>}
+
+          {errors.password && touched.password && errors.password  ? <label style={{position:'relative'}}><FieldError type="password"
             name="password"
             value={values.password}
             onChange={handleChange}
             onBlur={handleBlur}
              placeholder='Password'
              required/>
-     
-        <TextError>
-                  {errors.password && touched.password && errors.password }
-        </TextError>
-        </Wrap>
+             <TextError>{errors.password && touched.password && errors.password}</TextError>
+             </label>:
+             <label style={{position:'relative'}}>
+            <FieldStyled  type="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+             placeholder='Password'
+        
+            // style={{borderColor: ({ theme }) => theme.color.btnDark}}
+             required/>
+           {/* <IconEyeClosedStyle/> */}
+           {!errors.password && touched.password && <SuccessText>Password is secure</SuccessText>  }
+        </label> 
+            } 
+       </Wrap>
         <Button type='submit'>Login</Button>
         <Text>Don't have an account?<StyledNavLink to="/register">Register</StyledNavLink></Text>
      </FormStyled>
