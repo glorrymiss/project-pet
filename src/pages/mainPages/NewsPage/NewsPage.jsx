f81e92ecfdfb079dfb3175c104f7dc7adb39be4e
Iuriius <105295809+Iuriius@users.noreply.github.com>
2023-07-29 01:04:03 +0300

    pagination init

--- src/pages/mainPages/NewsPage/NewsPage.jsx
+++ src/pages/mainPages/NewsPage/NewsPage.jsx
@@ -1,17 +1,16 @@
-// import { Container } from 'components/Container/Container';
+import React, { useEffect, useState } from 'react';
 import Title from 'components/Title/Title';
 import theme from 'components/theme';
 import { useAuth } from 'hooks';
 import { Helmet } from 'react-helmet';
 import { FlexBox } from '../NoticesPage/NoticesPage.styled';
-import Pagination from 'components/Pagination/Pagination';
 import NewsList from 'components/NewsList/NewsList';
 import FormSearch from 'components/FormSearch/FormSearch';
-import { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Pagination } from '../../../components/Pagination/Pagination';
 
 const getNews = async page => {
-  const res = await axios.get('api/news', { page: page });
+  const res = await axios.get('api/news', { params: { page: page } });
   return res.data;
 };
 
@@ -20,7 +19,6 @@ const NewsPage = () => {
   const [newsList, setNewsList] = useState([]);
   const [quantityNews, setQuantityNews] = useState(0);
   const [page, setPage] = useState(1);
-  console.log('quantityNews', quantityNews);
 
   useEffect(() => {
     setPage(1);
@@ -43,7 +41,12 @@ const NewsPage = () => {
 
       <NewsList newsList={newsList} />
 
-      <Pagination page={page} setPage={setPage} quantity={quantityNews} />
+      <Pagination
+        currentPage={page}
+        totalPages={Math.ceil(quantityNews / 10)}
+        onPageChange={setPage}
+        paginationLength={5} // Adjust this number as per your preference
+      />
     </FlexBox>
   );
 };
