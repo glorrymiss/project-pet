f95314d4a96d7cc317f5e74e716560f54821f1f7
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-07-31 03:36:09 +0200

    Update newsPage

--- src/pages/mainPages/NewsPage/NewsPage.jsx
+++ src/pages/mainPages/NewsPage/NewsPage.jsx
@@ -8,6 +8,21 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Pagination } from '../../../components/Pagination/Pagination';
 import FormSearch from 'components/FormSearch/FormSearch';
+// import { NotFound404 } from 'components/NotFound/NotFound';
+import { Background } from 'components/Hero/Hero.styled';
+import { Cat, Links } from 'components/NotFound/NotFound.styled';
+import { CardTitle } from 'components/NewsList/NewsList.styled';
+/*
+/**|======================================
+/**| import images
+/**|======================================
+*/
+import notfound_sm_1x from 'images/NotFoundImages/notfound-sm@1x-1.webp';
+import notfound_sm_2x from 'images/NotFoundImages/notfound-sm@2x-2.webp';
+import notfound_md_1x from 'images/NotFoundImages/notfound-md@1x-1.webp';
+import notfound_md_2x from 'images/NotFoundImages/notfound-md@2x-2.webp';
+import notfound_lg_1x from 'images/NotFoundImages/notfound-lg@1x-1.webp';
+import notfound_lg_2x from 'images/NotFoundImages/notfound-lg@2x-2.webp';
 
 const getNews = async ({ page, limit, search = '' }) => {
   const res = await axios.get(
@@ -17,6 +32,7 @@ const getNews = async ({ page, limit, search = '' }) => {
 };
 
 const NewsPage = () => {
+  const [error, setError] = useState(null);
   const { currentTheme } = useAuth();
   const [newsList, setNewsList] = useState([]);
   const [quantityNews, setQuantityNews] = useState(null);
@@ -24,11 +40,20 @@ const NewsPage = () => {
   const [searchValue, setSearchValue] = useState('');
 
   useEffect(() => {
-    getNews({ page, limit: 6, search: searchValue }).then(res => {
-      const { quantityNews, news } = res;
-      setNewsList(news);
-      setQuantityNews(quantityNews);
-    });
+    if (newsList.length === 0) {
+      setError(true);
+    }
+  }, [newsList]);
+
+  useEffect(() => {
+    setError(null);
+    getNews({ page, limit: 6, search: searchValue })
+      .then(res => {
+        const { quantityNews, news } = res;
+        setNewsList(news);
+        setQuantityNews(quantityNews);
+      })
+      .catch(error => setError(error));
   }, [page, searchValue]);
 
   return (
@@ -41,14 +66,29 @@ const NewsPage = () => {
 
       <FormSearch setSearchValue={setSearchValue} />
 
-      <NewsList newsList={newsList} />
+      {error && (
+        <>
+          <Title>Ooops! Nothing found :(</Title>
 
-      <Pagination
-        currentPage={page}
-        totalPages={Math.ceil(quantityNews / 6)}
-        onPageChange={setPage}
-        paginationLength={5} // Adjust this number as per your preference
-      />
+          <Cat
+            srcSet={`${notfound_sm_1x} 280w, ${notfound_sm_2x} 560w, ${notfound_md_1x} 704w, ${notfound_md_2x} 1408w, ${notfound_lg_1x} 822w, ${notfound_lg_2x} 1644w`}
+            sizes="(min-width: 1280px) 1644px, (min-width: 1280px) 822px, (min-width: 768px) 1408px, (min-width: 768px) 704px,  100vw"
+            alt="not found page"
+            loading="lazy"
+          />
+        </>
+      )}
+
+      {!error && <NewsList newsList={newsList} />}
+
+      {!error && (
+        <Pagination
+          currentPage={page}
+          totalPages={Math.ceil(quantityNews / 6)}
+          onPageChange={setPage}
+          paginationLength={5} // Adjust this number as per your preference
+        />
+      )}
     </FlexBox>
   );
 };
